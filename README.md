2eb4b46cf7ae53430fa481d3b24a4a2c5c893c59
Mikhail Pak <mikhail.pak@tum.de>
2016-09-07 18:50:44 +0200

    Refactored and extended the install script
    Removed `thefuck` since it slows down terminal
    start considerably.

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # my-xubuntu-conf
-* Run `install.sh` and `configure.sh` (requires su rights).
-* Download [Fantasque Sans](https://github.com/belluzj/fantasque-sans/releases/latest)
-and [Source Code Pro](https://github.com/adobe-fonts/source-code-pro/releases/latest) fonts.
-* Install new fonts by typing `sudo fc-cache -f`.
-* Install Anaconda from [here](https://www.continuum.io/downloads#linux).
-* Install `thefuck` from [here](https://github.com/nvbn/thefuck#installation).
+* Most of the work is done by two shell scripts, `install.sh` and `configure.sh`.
+  * Download them by pasting this command into the shell:
+```sh
+wget https://raw.githubusercontent.com/mp4096/my-xubuntu-conf/master/install.sh \
+&& \
+wget https://raw.githubusercontent.com/mp4096/my-xubuntu-conf/master/configure.sh
+```
+  * Run them by typing
+```sh
+source install.sh && source configure.sh
+```
+* Install fancy typefaces:
+  * Download [Fantasque Sans](https://github.com/belluzj/fantasque-sans/releases/latest)
+    and [Source Code Pro](https://github.com/adobe-fonts/source-code-pro/releases/latest).
+  * Move them to `~/.fonts/`.
+  * Install them by typing `sudo fc-cache -f`.
+* Python3: Install Anaconda from [here](https://www.continuum.io/downloads#linux).
 * Setup git:
 ```
 git config --global user.name "ABC"
--- install.sh
+++ install.sh
@@ -1,14 +1,25 @@
+# Create directories
+mkdir ~/.fonts/ # prepare fonts installation
+mkdir ~/repos/  # folder for repos
+
 # Install Linux basics
 sudo apt install vim
-sudo apt install vim-gnome
 sudo apt install git
+sudo apt install htop
+sudo apt install gcc  # just in case
+sudo apt install make # just in case
+# Install nice apps
 sudo apt install git-cola
 sudo apt install meld
+sudo apt install vim-gnome
 sudo apt install keepassx
+sudo apt remove firefox # we'll install Pale Moon instead
 sudo apt install chromium-browser
-sudo apt install htop
-sudo apt remove firefox
 sudo apt install texlive-full
+# Install dependencies for ksuperkey
+sudo apt install libx11-dev
+sudo apt install libxtst-dev
+sudo apt install pkg-config
 
 # Install bash-it
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
@@ -19,5 +30,20 @@ wget -O "atom_installer.deb" "https://atom.io/download/deb"
 sudo dpkg --install "atom_installer.deb"
 rm "atom_installer.deb"
 
-# Prepare fonts installation
-mkdir "~/.fonts/"
+# Install ksuperkey
+git clone https://github.com/hanschen/ksuperkey.git ~/repos/ksuperkey
+cd ~/repos/ksuperkey
+make
+# Add it to autostart
+cat > ~/.config/autostart/ksuperkey.desktop <<EOF
+[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+Name=ksuperkey
+Comment=Map Windows button to Alt+F1
+Exec=$PWD/ksuperkey
+OnlyShowIn=XFCE;
+StartupNotify=false
+Terminal=false
+Hidden=false
+EOF
